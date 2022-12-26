371f200907d831d47122b2ac576f0c0d5aa63825
ccp84 <ccp84@icloud.com>
2022-12-26 14:52:49 +0000

    js>add api key check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-zkczXS9h0POwf4R_7qwnGcwDqdQ
+const API_KEY = 'zkczXS9h0POwf4R_7qwnGcwDqdQ';
+const API_URL = 'https://ci-jshint.herokuapp.com/api';
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+
+document.getElementById('status').addEventListener('click', (e) => getStatus(e));
+
+async function getStatus(e) {
+    let queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+    if(response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    let title = 'API Key Expiry Date';
+    let content = `<div>API Key expires on:</div> <div>${data.expiry}</div>`;
+
+    document.getElementById('resultsModalTitle').innerHTML = title;
+    document.getElementById('results-content').innerHTML = content;
+
+    resultsModal.show();
+}
